d68b81f4ff48f22fb4ead9a1d50df667f2c07105
vladpro42 <bondarev.vlad.01@gmail.com>
2022-07-30 15:11:54 +0300

    feat: add Greetings

--- momentum/main.js
+++ momentum/main.js
@@ -4,6 +4,37 @@
 const time = document.querySelector('.time');
 showTime();
 
+
+// День недели число месяц
+const timeDate = document.querySelector('.date');
+showDate();
+
+
+// Текст приветствия 
+const hiText = document.querySelector('.hello__text');
+const timeOfDay = getTimeOfDay()
+showGreeting();
+
+
+// Введенное имя
+const userName = document.querySelector('.name');
+
+//перед перезагрузкой или закрытием страницы (событие beforeunload) данные нужно сохранить
+
+window.addEventListener('beforeunload', () => {
+    localStorage.setItem('name', userName.value)
+});
+
+// перед загрузкой страницы (событие load) данные нужно восстановить и отобразить
+
+window.addEventListener('load', () => {
+    if(localStorage.getItem('name')) {
+        userName.value = localStorage.getItem('name');
+    }
+});
+
+
+
 // Время часы: минуты: секунды.
 function showTime() {
     const date = new Date(); // переменная с датой
@@ -12,11 +43,6 @@ function showTime() {
     setTimeout(showTime, 1000); // запуск самой себя с задержкой в 1с 
     return time // верни время
   }
-
-
-// День недели число месяц
-const timeDate = document.querySelector('.date');
-showDate();
 
 function showDate() {
     const date = new Date();
@@ -31,6 +57,23 @@ function showDate() {
     setTimeout(showDate, 1000);
     return timeDate;
 }
+
+function getTimeOfDay() {
+    
+    const date = new Date();
+    const hour = date.getHours();
+    if( hour >= 6 && hour < 12) return 'Доброе утро';
+    if( hour >= 12 && hour < 18) return 'Добрый день';
+    if( hour >= 18 && hour < 0) return 'Добрый вечер';
+    if( hour >= 0 && hour < 6) return 'Спокойной ночи';
+}
+
+function showGreeting() {
+    setTimeout(showGreeting, 60000);
+    console.log(timeOfDay)
+    return hiText.textContent = `${timeOfDay}`
+}
+
 
 
 
