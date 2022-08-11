8ced41653b2966c0f01d46b24d470a9712e91d38
vladpro42 <bondarev.vlad.01@gmail.com>
2022-08-11 14:54:40 +0300

    feat: audio player

---
+++ momentum/js/playList.js
@@ -0,0 +1,14 @@
+'use strict'
+ const playList = [
+    {      
+      title: 'XXXtentacion',
+      src: './music/XXX.mp3',
+      duration: '01:59'
+    },  
+    {      
+      title: 'Skyrim',
+      src: './music/skyrim.mp3',
+      duration: '03:57'
+    }
+  ]
+  export default playList; 
